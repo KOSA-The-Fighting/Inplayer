23365d0f1e3fc6b10b69836eb24fbb5135822563
fly5450 <fly5450@naver.com>
2024-10-31 16:31:41 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/Inplayer/InplayerApplication.java
@@ -0,0 +1,13 @@
+package com.Inplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Inplayer/InplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
